1db1e4d0d139c8084055ed4b5ec724819f2392c6
graffadamr <graff.adam.r@gmail.com>
2019-11-13 20:06:41 -0500

    first commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Capstone4
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/graffadamr/pen/Pooyowv](https://codepen.io/graffadamr/pen/Pooyowv).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,37 @@
+var itemNames = ["Cat", "Dog", "Fox"];
+var itemCosts = [25, 100, 150];
+var foxClicks = 0;
+var catClicks = 0;
+var dogClicks = 0;
+var totalCost = 0;
+
+
+for (i=0;i<itemNames.length;i++)
+{
+document.getElementById("list" + i).innerHTML =(itemNames[i] + ": $" +itemCosts[i] +  "<br >");
+}
+
+function addFoxToCart() {
+foxClicks += 1;  
+totalCost += 150;
+document.getElementById("cartFox").innerHTML = ("Foxes: " + foxClicks);
+document.getElementById("grandTotal").innerHTML = ("Grand total: $ " + totalCost);
+
+}
+
+function addCatToCart() {
+catClicks += 1;  
+totalCost += 25;
+document.getElementById("cartCat").innerHTML = ("Cats: " + catClicks);
+document.getElementById("grandTotal").innerHTML = ("Grand total: $ " + totalCost);
+
+}
+
+
+function addDogToCart() {
+dogClicks += 1;  
+totalCost += 100;
+document.getElementById("cartDog").innerHTML = ("Dogs: " + dogClicks);
+document.getElementById("grandTotal").innerHTML = ("Grand total: $ " + totalCost);
+}
+
